1f1623a2e03fb59f16ef4c409104e865d9551050
xhn2333 <120090453@link.cuhk.edu.cn>
2022-03-14 16:23:40 +0800

    init the repository

---
+++ HW3&4/Assignment3&4/build/CGL/CMakeFiles/CGL.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CGL.dir/src/base64.cpp.o"
+  "CMakeFiles/CGL.dir/src/color.cpp.o"
+  "CMakeFiles/CGL.dir/src/complex.cpp.o"
+  "CMakeFiles/CGL.dir/src/lodepng.cpp.o"
+  "CMakeFiles/CGL.dir/src/matrix3x3.cpp.o"
+  "CMakeFiles/CGL.dir/src/matrix4x4.cpp.o"
+  "CMakeFiles/CGL.dir/src/osdfont.cpp.o"
+  "CMakeFiles/CGL.dir/src/osdtext.cpp.o"
+  "CMakeFiles/CGL.dir/src/path.cpp.o"
+  "CMakeFiles/CGL.dir/src/quaternion.cpp.o"
+  "CMakeFiles/CGL.dir/src/tinyxml2.cpp.o"
+  "CMakeFiles/CGL.dir/src/vector2D.cpp.o"
+  "CMakeFiles/CGL.dir/src/vector3D.cpp.o"
+  "CMakeFiles/CGL.dir/src/vector4D.cpp.o"
+  "CMakeFiles/CGL.dir/src/viewer.cpp.o"
+  "libCGL.a"
+  "libCGL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ HW3&4/Assignment3&4/build/CMakeFiles/draw.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/draw.dir/src/drawrend.cpp.o"
+  "CMakeFiles/draw.dir/src/main.cpp.o"
+  "CMakeFiles/draw.dir/src/rasterizer.cpp.o"
+  "CMakeFiles/draw.dir/src/svg.cpp.o"
+  "CMakeFiles/draw.dir/src/svgparser.cpp.o"
+  "CMakeFiles/draw.dir/src/texture.cpp.o"
+  "CMakeFiles/draw.dir/src/transforms.cpp.o"
+  "CMakeFiles/draw.dir/src/triangulation.cpp.o"
+  "draw"
+  "draw.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/draw.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
