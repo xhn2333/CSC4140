1d809ecd95b78665eb8101cc5e9b2d85bfbb34af
xhn2333 <120090453@link.cuhk.edu.cn>
2022-03-16 20:20:22 +0800

    HW3&4 Task 3 updated

--- HW3&4/Assignment3&4/src/transforms.cpp
+++ HW3&4/Assignment3&4/src/transforms.cpp
@@ -4,33 +4,40 @@
 #include "CGL/vector2D.h"
 #include "CGL/vector3D.h"
 
-namespace CGL {
+namespace CGL
+{
 
-Vector2D operator*(const Matrix3x3 &m, const Vector2D &v) {
-	Vector3D mv = m * Vector3D(v.x, v.y, 1);
-	return Vector2D(mv.x / mv.z, mv.y / mv.z);
-}
+    Vector2D operator*(const Matrix3x3 &m, const Vector2D &v)
+    {
+        Vector3D mv = m * Vector3D(v.x, v.y, 1);
+        return Vector2D(mv.x / mv.z, mv.y / mv.z);
+    }
 
-Matrix3x3 translate(float dx, float dy) {
-	// Part 3: Fill this in.
+    Matrix3x3 translate(float dx, float dy)
+    {
+        // Part 3: Fill this in.
 
+        return Matrix3x3(1, 0, dx,
+                         0, 1, dy,
+                         0, 0, 1);
+    }
 
-	return Matrix3x3();
-}
+    Matrix3x3 scale(float sx, float sy)
+    {
+        // Part 3: Fill this in.
 
-Matrix3x3 scale(float sx, float sy) {
-	// Part 3: Fill this in.
+        return Matrix3x3(sx, 0, 0,
+                         0, sy, 0,
+                         0, 0, 1);
+    }
 
-
-	return Matrix3x3();
-}
-
-// The input argument is in degrees counterclockwise
-Matrix3x3 rotate(float deg) {
-	// Part 3: Fill this in.
-
-
-	return Matrix3x3();
-}
+    // The input argument is in degrees counterclockwise
+    Matrix3x3 rotate(float deg)
+    {
+        // Part 3: Fill this in.
+        return Matrix3x3(cos(deg * PI / 180), sin(deg * PI / 180), 0,
+                         -sin(deg * PI / 180), cos(deg * PI / 180), 0,
+                         0, 0, 1);
+    }
 
 }
