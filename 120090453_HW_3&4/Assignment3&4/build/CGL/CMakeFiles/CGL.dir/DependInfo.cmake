350c0cf6f153e8a7b7886164af630d472afa2321
xhn2333 <120090453@link.cuhk.edu.cn>
2022-03-27 22:09:32 +0800

    HW3&4 name updated

---
+++ 120090453_HW_3&4/Assignment3&4/build/CGL/CMakeFiles/CGL.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/CGL/src/base64.cpp" "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CGL/CMakeFiles/CGL.dir/src/base64.cpp.o"
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/CGL/src/color.cpp" "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CGL/CMakeFiles/CGL.dir/src/color.cpp.o"
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/CGL/src/complex.cpp" "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CGL/CMakeFiles/CGL.dir/src/complex.cpp.o"
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/CGL/src/lodepng.cpp" "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CGL/CMakeFiles/CGL.dir/src/lodepng.cpp.o"
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/CGL/src/matrix3x3.cpp" "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CGL/CMakeFiles/CGL.dir/src/matrix3x3.cpp.o"
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/CGL/src/matrix4x4.cpp" "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CGL/CMakeFiles/CGL.dir/src/matrix4x4.cpp.o"
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/CGL/src/osdfont.cpp" "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CGL/CMakeFiles/CGL.dir/src/osdfont.cpp.o"
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/CGL/src/osdtext.cpp" "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CGL/CMakeFiles/CGL.dir/src/osdtext.cpp.o"
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/CGL/src/path.cpp" "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CGL/CMakeFiles/CGL.dir/src/path.cpp.o"
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/CGL/src/quaternion.cpp" "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CGL/CMakeFiles/CGL.dir/src/quaternion.cpp.o"
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/CGL/src/tinyxml2.cpp" "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CGL/CMakeFiles/CGL.dir/src/tinyxml2.cpp.o"
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/CGL/src/vector2D.cpp" "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CGL/CMakeFiles/CGL.dir/src/vector2D.cpp.o"
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/CGL/src/vector3D.cpp" "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CGL/CMakeFiles/CGL.dir/src/vector3D.cpp.o"
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/CGL/src/vector4D.cpp" "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CGL/CMakeFiles/CGL.dir/src/vector4D.cpp.o"
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/CGL/src/viewer.cpp" "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CGL/CMakeFiles/CGL.dir/src/viewer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../CGL/include"
+  "../CGL/include/CGL"
+  "/usr/include/freetype2"
+  "../CGL/deps/glew/include"
+  "../CGL/deps/glfw/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CGL/deps/glew/CMakeFiles/glew.dir/DependInfo.cmake"
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 120090453_HW_3&4/Assignment3&4/build/CMakeFiles/draw.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/src/drawrend.cpp" "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CMakeFiles/draw.dir/src/drawrend.cpp.o"
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/src/main.cpp" "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CMakeFiles/draw.dir/src/main.cpp.o"
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/src/rasterizer.cpp" "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CMakeFiles/draw.dir/src/rasterizer.cpp.o"
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/src/svg.cpp" "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CMakeFiles/draw.dir/src/svg.cpp.o"
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/src/svgparser.cpp" "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CMakeFiles/draw.dir/src/svgparser.cpp.o"
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/src/texture.cpp" "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CMakeFiles/draw.dir/src/texture.cpp.o"
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/src/transforms.cpp" "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CMakeFiles/draw.dir/src/transforms.cpp.o"
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/src/triangulation.cpp" "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CMakeFiles/draw.dir/src/triangulation.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../CGL/include"
+  "../CGL/deps/glew/include"
+  "../CGL/deps/glfw/include"
+  "../CGL/include/CGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CGL/CMakeFiles/CGL.dir/DependInfo.cmake"
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CGL/deps/glew/CMakeFiles/glew.dir/DependInfo.cmake"
+  "/home/xhn/Desktop/CSC4140/HW3&4/Assignment3&4/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
